fce8dcb05266693242ef2bf9c4b0c0370ca53bf2
Cristi Scheye <cristi.scheye@gmail.com>
2015-06-03 09:22:29 -0700

    add color pickers and text shadow style

--- view/index.jsx
+++ view/index.jsx
@@ -1,6 +1,7 @@
 import React, {PropTypes, Component} from 'react'
 import _ from 'lodash'
-const namespace = 'view'
+import ColorPicker from 'react-color-picker'
+const namespace = 'shadowPlayground'
 
 const fontFamilies = {
     serif: ['Times', 'Times New Roman', 'Georgia', 'serif']
@@ -13,15 +14,41 @@ const fontFamilies = {
 export default class View extends Component {
   constructor () {
     super()
+
     this.state = {
-      text: 'hi'
+      text: 'Text Shadow Playground'
+      , shadow: {
+        color: '#000'
+        , offsetX: '1px'
+        , offsetY: '2px'
+        , blurRadius: '2px'
+      }
       , style: {
-        color: 'rgb(25,100,34)'
+        color: '#000'
+        , backgroundColor: '#FFF'
         , fontFamily: fontFamilies.serif
       }
     }
+
+    this.setShadow()
   }
 
+// helpers
+  setShadow () {
+    let {shadow} = this.state
+    let {style} = this.state
+
+    console.log('shadow', shadow)
+    console.log('style', style)
+
+    style.textShadow = `${shadow.offsetX} ${shadow.offsetY} ${shadow.blurRadius} ${shadow.color}`
+
+
+    console.log('style', style)
+    this.setState({style})
+  }
+
+// on change methods
   onChange (field, e) {
     var newState = {}
     newState[field] = e.target.value
@@ -38,11 +65,27 @@ export default class View extends Component {
     this.setState({style})
   }
 
+  onColorDrag (field, color) {
+    let {style} = this.state
+    let {shadow} = this.state
+
+    if (field === 'shadowColor') {
+      shadow.color = color
+      this.setState({shadow})
+      this.setShadow()
+    }
+    else {
+      style[field] = color
+    }
+    this.setState({style})
+  }
+
+// subviews
   fontRadioButtons () {
     var self = this
     return _.map(fontFamilies, function (_fonts, family){
       return (
-        <p>
+        <li style={{fontFamily: family}}>
           <input
             type="radio"
             name="fontFamily"
@@ -51,7 +94,7 @@ export default class View extends Component {
           >
             {family}
           </input>
-        </p>
+        </li>
         )
     })
   }
@@ -60,20 +103,32 @@ export default class View extends Component {
     return (<div className={namespace}>
       <h1 className={`${namespace}-title`}>Text Shadow Playground</h1>
 
-      <input onChange={_.bind(this.onChange, this, 'text')} type="text" />
-
       <h2 style={this.state.style}>{this.state.text}</h2>
 
-      <ul>
-        {this.fontRadioButtons.call(this)}
-      </ul>
+      <input onChange={_.bind(this.onChange, this, 'text')} type="text" />
+      <ul>{this.fontRadioButtons.call(this)}</ul>
 
-      // display whatever is in text, apply css here
-      // input field for text
-      // input fields to change...
-        // text shadow
-        // background color
-        // text color
+      <h3>Text Color</h3>
+      <ColorPicker
+        className={`${namespace}-textColorPicker`}
+        defaultValue={this.state.style.color}
+        onDrag={_.bind(this.onColorDrag, this, 'color')}/>
+
+      <h3>Background Color</h3>
+      <ColorPicker
+        className={`${namespace}-backgroundColorPicker`}
+        defaultValue={this.state.style.backgroundColor}
+        onDrag={_.bind(this.onColorDrag, this, 'backgroundColor')}/>
+
+      <h3>Shadow Color</h3>
+      <ColorPicker
+        className={`${namespace}-shadowColorPicker`}
+        defaultValue={this.state.style.shadowColor}
+        onDrag={_.bind(this.onColorDrag, this, 'shadowColor')}/>
+
+
+      <h3>CSS</h3>
+      <p>{JSON.stringify(this.state.style)}</p>
     </div>)
   }
 }
