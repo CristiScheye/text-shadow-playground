3588b9ff7beb568a0f58e988518af942ec9be9f5
Cristi Scheye <cristi.scheye@gmail.com>
2016-04-10 15:32:50 -0700

    refactor UI

--- src/view/index.jsx
+++ src/view/index.jsx
@@ -1,9 +1,7 @@
 import React, {Component} from 'react'
-import ColorPicker from 'react-color-picker'
 import bind from 'lodash/function/bind'
 import map from 'lodash/collection/map'
-
-const namespace = 'shadowPlayground'
+import css from './index.css'
 
 const fontFamilies = {
     serif: ['Times', 'Times New Roman', 'Georgia', 'serif']
@@ -17,207 +15,151 @@ export default class View extends Component {
   constructor () {
     super()
 
-    this.state = {
-      text: 'Text Shadow Playground'
-      , shadow: {
-        color: '#000'
-        , offsetX: '0.1em'
-        , offsetY: '0.1em'
-        , blurRadius: '0.1em'
-      }
-      , style: {
-        color: '#000'
-        , backgroundColor: '#FFF'
-        , fontFamily: fontFamilies.serif
-      }
-      , activeColor: 'style.color'
-    }
+    this.onChange = this.onChange.bind(this)
+    this.setShadow = this.setShadow.bind(this)
 
+    this.state = {
+      content: 'Hello World'
+      , shadowColor: 'rgba(255,255,255,0.1)'
+      , offsetX: '3px'
+      , offsetY: '3px'
+      , blurRadius: '2px'
+      , textColor: 'rgba(0,0,0,0.6)'
+      , backgroundColor: '#222'
+      , fontFamily: fontFamilies.sansserif
+    }
+  }
+
+  componentWillMount () {
     this.setShadow()
   }
 
 // helpers
   setShadow () {
-    let {shadow} = this.state
-    let {style} = this.state
+    const {offsetX, offsetY, blurRadius, shadowColor} = this.state
+    const textShadow = `${offsetX} ${offsetY} ${blurRadius} ${shadowColor}`
 
-    style.textShadow = `${shadow.offsetX} ${shadow.offsetY} ${shadow.blurRadius} ${shadow.color}`
-
-    this.setState({style})
+    this.setState({textShadow})
   }
 
-// on change methods
-  onTextChange (e) {
-    let text = e.target.value
-
-    if (!text.length) {
-      text = 'Text Shadow Playground'
-    }
-
-    this.setState({text})
-  }
-
-  onStyleChange (e) {
-    var value = e.target.value
-      , name = e.target.name
-      , style = this.state.style
-
-    style[name] = fontFamilies[value]
-    this.setState({style})
-  }
-  onShadowChange (field, value) {
-    const {shadow} = this.state
-    // colorpicker sends string, others send event
-    if (field !== 'color') {
-      value = value.target.value + 'em'
-    }
-
-    shadow[field] = value
-    this.setState({shadow})
-    this.setShadow()
-    console.log(this.state)
-  }
-
-  onColorDrag (color) {
-    let {activeColor} = this.state
-    let styleOrShadow = activeColor.split('.')[0]
-    let attr = activeColor.split('.')[1]
-
-    let subState = this.state[styleOrShadow]
-    subState[attr] = color
-
-    this.setState(subState)
-
-    if (styleOrShadow === 'shadow') {
-      this.setShadow()
-    }
-  }
-
-  onClickColor (field) {
-    this.setState({activeColor: field})
-  }
-
-
-// subviews
-  fontRadioButtons () {
-    var self = this
-    return map(fontFamilies, function (_fonts, family){
-      return (
-        <label
-          className={`${namespace}-fontInput`}
-          style={{fontFamily: family}}>
-          <input
-            type="radio"
-            name="fontFamily"
-            value={family}
-            onChange={self.onStyleChange.bind(self)}
-          >
-            {family}
-          </input>
-        </label>
-        )
-    })
-  }
-
-  colorBtn (label, color) {
-      let attrs = color.split('.')
-      let style = {
-        backgroundColor: this.state[attrs[0]][attrs[1]]
-      }
-
-      let className = `${namespace}-colorBlock`
-      if (color === this.state.activeColor) {
-        className += ' active'
-      }
-
-      return <div
-                className={className}
-                onClick={this.onClickColor.bind(this, color)}>
-                <span style={style}></span>
-                {label}
-              </div>
+  onChange (e) {
+    const {name, value} = e.target
+    this.setState({[name]: value}, this.setShadow)
   }
 
   render () {
-    return (<div className={namespace}>
-      <div  className={`${namespace}-demo`}>
-        <h1 style={this.state.style}>{this.state.text}</h1>
+    const demoStyle = {
+      color: this.state.textColor
+      , backgroundColor: this.state.backgroundColor
+      , textShadow: this.state.textShadow
+      , fontFamily: this.state.fontFamily
+    }
+
+    return (<div className={css.page}>
+      <div  className={css.demo}>
+        <h1 style={demoStyle}>{this.state.content}</h1>
       </div>
 
-      <input
-        className={`${namespace}-textInput`}
-        onChange={bind(this.onTextChange, this)}
-        type="text"
-        placeholder="Your Text Here"/>
+      <div className={css.playground}>
 
-      <div className={`${namespace}-fontStyles`}>
-        <div className={`${namespace}-fonts`}>
-          <label className={`${namespace}-label--section`}>Font Family</label>
-            {this.fontRadioButtons.call(this)}
-        </div>
-
-        <div>
-          <label className={`${namespace}-label--section`}>
-            Text Shadow Properties (em)
-          </label>
-          <div className={`${namespace}-shadows`}>
-            <label>Offset-X
+        <div className={css.form}>
+          <div className={css.inputInline}>
+            <label>Demo Text</label>
             <input
-              onChange={bind(this.onShadowChange, this, 'offsetX')}
-              type="number"
-              min="-5"
-              max="5"
-              step="0.01"
-              defaultValue="0.1"
+              className={css.input}
+              onChange={this.onChange}
+              type="text"
+              name="content"
+              value={this.state.content}
+              placeholder="Your Text Here"/>
+          </div>
+
+
+          <div className={css.inputInline}><label>Font Family</label>
+            <select
+              className={css.input}
+              defaultValue="sansserif"
+              name="fontFamily"
+              value={this.state.fontFamily}
+              onChange={this.onChange}>
+              {map(fontFamilies, (fonts, label) => {
+                return <option
+                  key={label}
+                  value={fonts}>
+                  {label}
+                </option>
+              })}
+            </select>
+          </div>
+
+
+          <div className={css.inputInline}><label>Offset-X</label>
+            <input
+              className={css.input}
+              onChange={this.onChange}
+              type="text"
+              value={this.state.offsetX}
               name="offsetX" />
-            </label>
+          </div>
 
-            <label>Offset-Y
+
+          <div className={css.inputInline}><label>Offset-Y</label>
             <input
-              onChange={bind(this.onShadowChange, this, 'offsetY')}
-              type="number"
-              min="-5"
-              max="5"
-              step="0.01"
-              defaultValue="0.1"
-              name="offsetY" />
-            </label>
-
-            <label>Blur Radius
-              <input
-                onChange={bind(this.onShadowChange, this, 'blurRadius')}
-                type="number"
-                min="0"
-                max="5"
-                step="0.01"
-                defaultValue="0.1"
-                name="blurRadius" />
-            </label>
+              className={css.input}
+              onChange={this.onChange}
+              type="text"
+              name="offsetY"
+              value={this.state.offsetY} />
           </div>
+
+
+          <div className={css.inputInline}><label>Blur Radius</label>
+            <input
+              className={css.input}
+              onChange={this.onChange}
+              type="text"
+              name="blurRadius"
+              value={this.state.blurRadius} />
+          </div>
+
+
+          <div className={css.inputInline}><label>Text Color</label>
+            <input
+              className={css.input}
+              onChange={this.onChange}
+              type="text"
+              name="textColor"
+              value={this.state.textColor} />
+          </div>
+
+
+          <div className={css.inputInline}><label>Background Color</label>
+            <input
+              className={css.input}
+              onChange={this.onChange}
+              type="text"
+              name="backgroundColor"
+              value={this.state.backgroundColor} />
+          </div>
+
+
+          <div className={css.inputInline}><label>Shadow Color</label>
+            <input
+              className={css.input}
+              onChange={this.onChange}
+              type="text"
+              name="shadowColor"
+              value={this.state.shadowColor} />
+          </div>
+
         </div>
 
-        <div>
-          <label className={`${namespace}-label--section`}>Colors</label>
-          <div className={`${namespace}-colors`}>
-
-            <div className={`${namespace}-colorLabels`}>
-              {this.colorBtn.call(this, 'Text', 'style.color')}
-              {this.colorBtn.call(this, 'Background', 'style.backgroundColor')}
-              {this.colorBtn.call(this, 'Shadow', 'shadow.color')}
-            </div>
-
-            <ColorPicker
-              defaultValue={this.state.style.color}
-              onDrag={this.onColorDrag.bind(this)}/>
-          </div>
+        <div className={css.code}>
+          <pre><code>{JSON.stringify(demoStyle, null, 2)}</code></pre>
         </div>
 
       </div>
 
-      <div className={`${namespace}-code`}>
-        <h3>Generated CSS</h3>
-        <pre><code>{JSON.stringify(this.state.style, null, 2)}</code></pre>
-      </div>
     </div>)
   }
 }
